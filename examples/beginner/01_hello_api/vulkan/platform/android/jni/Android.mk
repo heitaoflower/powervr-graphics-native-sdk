7329326a85bf67ce9bbba4501de87e3c804f3be3
hogan.yin <hogan.yin@vrviu.com>
2017-07-27 08:45:46 -0700

    1. Update .gitignore file. 2. Add vulkan_hello_api example.

---
+++ examples/beginner/01_hello_api/vulkan/platform/android/jni/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)/../../..
+PVRSDKDIR := $(realpath $(call my-dir)/../../../../../../..)
+
+
+# Module VulkanHelloAPI
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := vulkan_hello_api
+
+### Add all source file names to be included in lib separated by a whitespace
+LOCAL_SRC_FILES := vulkan_hello_api_android.cpp
+
+LOCAL_C_INCLUDES := $(PVRSDKDIR)/builds/include
+
+LOCAL_LDLIBS := -llog \
+                -landroid 
+
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
